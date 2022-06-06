1cc63ac14db87e6086ed9e12416dd6a0de419411
LudovicaBindi <ludovica.bindi@mail.polimi.it>
2022-06-06 19:04:01 +0200

    Updated the data. Added new reference scenario and saving of the results of optimization.

--- i hate this project.py
+++ i hate this project.py
@@ -1,13 +1,21 @@
 from ema_workbench import (Model, RealParameter, TimeSeriesOutcome,
                            perform_experiments, ema_logging,Policy,MultiprocessingEvaluator,SequentialEvaluator)
+from ema_workbench.em_framework.points import Scenario
 
 from problem_formulation import get_model_for_problem_formulation
 
 # setting up the model as usual
-ema_logging.log_to_stderr(ema_logging.INFO)
+if __name__ == '__main__':
 
-model, planning_steps = get_model_for_problem_formulation(2)
+    dike_model, planning_steps = get_model_for_problem_formulation(3)
 
-with SequentialEvaluator(model) as evaluator:
-    results1 = evaluator.optimize(nfe=2, searchover='levers',
-                                 epsilons=[0.1,]*len(model.outcomes))
+    lower_bound_ref_sce = {}
+    for uncertainty in dike_model.uncertainties:
+        lower_bound_ref_sce[uncertainty.name] = uncertainty.lower_bound
+
+    ema_logging.log_to_stderr(ema_logging.INFO)
+
+    with MultiprocessingEvaluator(dike_model) as evaluator:
+        results1 = evaluator.optimize(nfe=5, reference=Scenario(**lower_bound_ref_sce),
+                                      searchover='levers',
+                                      epsilons=[0.1,]*len(dike_model.outcomes))
