59740dacdcfe2d18101f64ec4c511d2b304ef524
LudovicaBindi <ludovica.bindi@mail.polimi.it>
2022-06-17 11:23:44 +0200

    ste2 file cleaned.

--- final_step2_reevaluate_policies.py
+++ final_step2_reevaluate_policies.py
@@ -1,16 +1,19 @@
-import pandas as pd
+# import needed libraries
 
-
-import pickle
-import time  # to keep track of the runtime
-
+# ema_workbench components needed
 from ema_workbench import Policy, MultiprocessingEvaluator
 
 # model functions needed
 from problem_formulation import get_model_for_problem_formulation
 
+# other libraries needed
+import pandas as pd # to deal with csv files
+import pickle # to save dictionaries
+import time  # to keep track of the runtime
+
 if __name__ == '__main__':
-    # read the data
+
+    # read the policies found via the first optimization
     results = pd.read_csv('intermediate outputs/step1 - first optimization output(100000,[0.1]).csv')
     results = results.drop(columns=['Unnamed: 0'])  # cleaning
 
@@ -25,8 +28,9 @@ if __name__ == '__main__':
     for o in dike_model.levers:
         to_drop.remove(o.name)
 
-    # select some policies
-    # logical = results['A.1_DikeIncrease 1'] < 4
+    # select some policies. In our case we saw that the policies we got our a small enough number and cause low enough
+    # number of deaths in our reference scenario that are in line with our client's values
+    # logical = results['A.4_Expected Number of Deaths'] < 0.0012
     # policies = results[logical]
     policies = results  # keep everything
     policies = policies.drop([column for column in to_drop], axis=1);
@@ -36,15 +40,17 @@ if __name__ == '__main__':
     for i, policy in policies.iterrows():
         policies_to_evaluate.append(Policy(str(i), **policy.to_dict()))
 
+    # print information of starting time on the console
     start_time = time.time()
     print('Runtime started')
 
-    # run the experiments
-    n_scenarios = 100
+    # run the experiments: each policy will be evaluated in all the sampled scenarios
+    n_scenarios = 100 # number of scenarios that will be sampled
     with MultiprocessingEvaluator(dike_model) as evaluator:
         experiments, outcomes = evaluator.perform_experiments(n_scenarios,
                                                 policies_to_evaluate)
 
+    # print information of ending time on the console
     end_time = time.time()
     print('Runtime ended with duration of', str(end_time - start_time))
 
